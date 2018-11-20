52c5c5b99963809d1c922875a0aece6d98fa840e
Seb Dalgarno <seb@poissonconsulting.ca>
2018-11-19 17:47:46 -0800

    iso50 inspired colours

--- hex.R
+++ hex.R
@@ -20,22 +20,22 @@ hex_sf <- st_as_sf(hexd, coords = c("x", "y")) %>%
   st_cast("LINESTRING") %>%
   st_cast("POLYGON")
 
-buffers <- seq(-0.05, -0.85, -0.14)
+buffers <- seq(-0.05, -0.85, -0.12)
 hexes <- do.call("rbind", lapply(buffers, function(x){
   st_buffer(hex_sf, x) %>%
     mutate(id = x)
 }))
 
-hex_tint <- "#1381c2"
-hex_text <- "#05082c"
-hex_outline <- "#05082c"
+hex_tint <- "#fa6a5c"
+hex_text <- "#263e51"
+hex_outline <- "#263e51"
 family <- "montserrat"
-
+pal <- rev(tinter::tinter(hex_tint, steps = length(buffers) , crop = 2, direction = "both"))[-(1:5)]
 # create more polygons
 gp <- ggplot() +
   geom_sf(data = hexes[1,], color = hex_outline, size = 7) +
   geom_sf(data = hexes[-nrow(hexes),], aes(fill = id), alpha = 1, color = "transparent", size = 0.01) +
-  scale_fill_gradientn(colours = tinter::tint(hex_tint, steps = length(buffers) -1, crop = 1)) +
+  scale_fill_gradientn(colours = pal) +
   geom_text(aes(x = 1, y = 1, label = "tinter"), size = 31, colour = hex_text, family = family) +
   geom_url(url = "github.com/poissonconsulting/tinter", family = family, size = 3, vjust = 0.1, color = hex_text) +
   theme_transparent() +
