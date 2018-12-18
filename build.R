74672e80fec08c9f7795cb02755de8bfa9becbe6
Joe Thorley <joe@poissonconsulting.ca>
2018-12-17 16:53:14 -0800

    update webpage

--- build.R
+++ build.R
@@ -2,6 +2,8 @@ source("hex.R")
 devtools::test()
 devtools::document()
 # knitr::knit("README.Rmd")
-if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
-pkgdown::build_site()
+if(FALSE) {
+  if(file.exists("DESCRIPTION")) unlink("docs", recursive = TRUE)
+  pkgdown::build_site()
+}
 devtools::check()
