6ee4aea5172a865d9c04cedcfe30d579a8cd5644
joethorley <joe@poissonconsulting.ca>
2020-03-31 16:51:03 -0700

    style files and do roxygen2markdown

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,23 @@
-shade <- function(x, steps, crop){
+shade <- function(x, steps, crop) {
   shade <- rev(grDevices::colorRampPalette(c(x, "black"))(steps + 1))
-  if(crop == 0){
+  if (crop == 0) {
     return(rev(shade))
   }
   rev(shade[-(seq_len(crop))])
 }
 
-tint <- function(x, steps, crop){
+tint <- function(x, steps, crop) {
   tint <- rev(grDevices::colorRampPalette(c(x, "white"))(steps + 1))
-  if(crop == 0){
+  if (crop == 0) {
     return(tint)
   }
   tint[-(seq_len(crop))]
 }
 
-check_colour <- function(x){
+check_colour <- function(x) {
   chk_string(x)
-  res <- try(grDevices::col2rgb(x),silent = TRUE)
-  if(class(res) == "try-error")
+  res <- try(grDevices::col2rgb(x), silent = TRUE)
+  if (class(res) == "try-error") {
     stop(x, " is not a valid color", call. = FALSE)
+  }
 }
-
